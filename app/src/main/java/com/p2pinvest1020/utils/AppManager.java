8a27319fa5897fd47e6c169a18da5b81fc28b7f1
wangfeilong <418593670@qq.com>
2017-03-11 11:54:57 +0800

    AppManager

---
+++ app/src/main/java/com/p2pinvest1020/utils/AppManager.java
@@ -0,0 +1,37 @@
+package com.p2pinvest1020.utils;
+
+import android.app.Activity;
+
+import java.util.Stack;
+
+/**
+ * Created by Administrator on 2017/3/11.
+ */
+
+public class AppManager {
+
+    /**
+     * 统一应用程序中所有的Activity的栈管理（单例）
+     * 涉及到activity的添加、删除指定、删除当前、删除所有、返回栈大小的方法
+     */
+
+
+
+    private AppManager(){};
+
+    private static  AppManager appManager = new AppManager();
+
+    public static AppManager getInstance(){
+        return appManager;
+    }
+
+    private Stack<Activity> stack = new Stack<>();
+    public void addActivity(Activity activity){
+        //校验
+        if (activity != null){
+            stack.add(activity);
+        }
+    }
+
+
+}
