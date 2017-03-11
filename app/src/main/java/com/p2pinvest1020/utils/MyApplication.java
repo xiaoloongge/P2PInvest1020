837f6aababc20329dca6c8c9c0f522f3219f0d4a
wangfeilong <418593670@qq.com>
2017-03-11 10:54:08 +0800

    uiutils

---
+++ app/src/main/java/com/p2pinvest1020/utils/MyApplication.java
@@ -0,0 +1,83 @@
+package com.p2pinvest1020.utils;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Handler;
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2017/3/11.
+ */
+
+public class MyApplication extends Application {
+
+    private static Context context;
+    private static Thread mainThread;
+    private static int  threadid;
+    private static Handler handler;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = this;
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+
+    public static Thread getMainThread() {
+        return mainThread;
+    }
+
+    public static int getThreadid() {
+        return threadid;
+    }
+
+    public static Handler getHandler() {
+        return handler;
+    }
+
+    /*
+    * 1、onCreate（） 在创建应用程序时创建
+
+      2、onTerminate（） 在模拟环境下执行。当终止应用程序对象时调用，不保证一定被调用，
+      当程序是被内核终止以便为其他应用程序释放资源，
+      那么将不会提醒，并且不调用应用程序的对象的onTerminate方法而直接终止进程。
+
+3、onLowMemory（） 低内存时执行。
+好的应用程序一般会在这个方法里面释放一些不必要的资源来应付当后台程序已经终止，
+前台应用程序内存还不够时的情况。
+
+4、onConfigurationChanged（Configuration newConfig） 配置改变时触发这个方法。
+
+5、onTrimMemory（int level）程序在进行内存清理时执行​
+    *
+    *
+    * */
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+        Log.i("appliction", "onTrimMemory: ");
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        Log.i("appliction", "onLowMemory: ");
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Log.i("appliction", "onConfigurationChanged: ");
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        Log.i("appliction", "onTerminate: ");
+    }
+}
---
+++ app/src/main/java/com/p2pinvest1020/utils/UiUtils.java
@@ -0,0 +1,45 @@
+package com.p2pinvest1020.utils;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017/3/11.
+ *
+ *
+ * 和UI相关的一些操作类
+ */
+
+public class UiUtils {
+
+
+    public static Context getContext(){
+        return MyApplication.getContext();
+    }
+
+    public static View getView(int layoutid){
+        return View.inflate(getContext(),layoutid,null);
+    }
+
+    public static int getColor(int color){
+        return getContext().getResources().getColor(color);
+    }
+
+    public static String[] getStringArrary(int stringid){
+        return getContext().getResources().getStringArray(stringid);
+    }
+
+    //与屏幕分辨率相关的
+    public static int dp2px(int dp){
+        float density = getContext().getResources().getDisplayMetrics().density;
+        return (int) (density * dp + 0.5);
+
+    }
+
+    public static int px2dp(int px){
+        float density = getContext().getResources().getDisplayMetrics().density;
+        return (int) (px / density + 0.5);
+    }
+
+    
+}
