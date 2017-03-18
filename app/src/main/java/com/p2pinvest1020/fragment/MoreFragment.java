984dfeedb789fd80f7b6cee324ada4e55eb3299f
wangfeilong <418593670@qq.com>
2017-03-18 11:11:17 +0800

    导入手势密码jar包

--- app/src/main/java/com/p2pinvest1020/fragment/MoreFragment.java
+++ app/src/main/java/com/p2pinvest1020/fragment/MoreFragment.java
@@ -1,18 +1,49 @@
 package com.p2pinvest1020.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import com.p2pinvest1020.R;
+import com.p2pinvest1020.activity.GestureEditActivity;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by Administrator on 2017/3/10.
  */
 public class MoreFragment extends BaseFragment {
 
+
+    @Bind(R.id.base_title)
+    TextView baseTitle;
+    @Bind(R.id.base_back)
+    ImageView baseBack;
+    @Bind(R.id.base_setting)
+    ImageView baseSetting;
+    @Bind(R.id.tv_more_regist)
+    TextView tvMoreRegist;
+    @Bind(R.id.toggle_more)
+    ToggleButton toggleMore;
+    @Bind(R.id.tv_more_reset)
+    TextView tvMoreReset;
+    @Bind(R.id.tv_more_phone)
+    TextView tvMorePhone;
+    @Bind(R.id.rl_more_contact)
+    RelativeLayout rlMoreContact;
+    @Bind(R.id.tv_more_fankui)
+    TextView tvMoreFankui;
+    @Bind(R.id.tv_more_share)
+    TextView tvMoreShare;
+    @Bind(R.id.tv_more_about)
+    TextView tvMoreAbout;
 
     @Override
     protected void initListener() {
@@ -22,15 +53,27 @@ public class MoreFragment extends BaseFragment {
     @Override
     protected void initData(String json) {
 
+        initTitle();
+
+        //设置手势密码
+        startActivity(new Intent(getActivity(), GestureEditActivity.class));
+    }
+
+    private void initTitle() {
+
+       baseBack.setVisibility(View.GONE);
+        baseTitle.setText("设置更多");
+        baseSetting.setVisibility(View.GONE);
     }
 
     @Override
     public int getLayoutid() {
-        return 0;
+        return R.layout.fragment_more;
     }
 
     @Override
     public String getChildUrl() {
         return null;
     }
+
 }
